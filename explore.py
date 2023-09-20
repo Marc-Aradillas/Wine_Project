4414b0e0b273562c62fe6555717fa3adbf24cd1b
Marc Aradillas <aradillasm@gmail.com>
2023-09-20 16:49:38 -0500

    modified

---
+++ explore.py
@@ -0,0 +1,77 @@
+# imported libs
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+
+
+def eval_p(p, a=0.05, decimal_places=2):
+    """
+    Evaluate the p-value and print the result of hypothesis testing.
+
+    Args:
+        p (float): The p-value to evaluate.
+        a (float, optional): The significance level (default is 0.05).
+        decimal_places (int, optional): The number of decimal places for formatting p-value (default is 2).
+
+    Returns:
+        None
+    """
+    formatted_p = "{:.{precision}e}".format(p, precision=decimal_places)
+    if p < a:
+        print(f'\nWe reject the null hypothesis with a p-value of {formatted_p}.')
+    else:
+        print(f'\nWe failed to reject the null hypothesis with a p-value of {formatted_p}.')
+
+
+
+def chi2_and_visualize(df, cat_var, target, a=0.05, decimal_places=2):
+    """
+    Perform chi-squared test and visualize the results.
+
+    Args:
+        df (DataFrame): The DataFrame containing the data.
+        cat_var (str): The categorical variable to be tested.
+        target (str): The target variable for the chi-squared test.
+        a (float, optional): The significance level (default is 0.05).
+        decimal_places (int, optional): The number of decimal places for formatting p-value (default is 2).
+
+    Returns:
+        None
+    """
+    observed = pd.crosstab(df[cat_var], df[target])
+    chi2, p, degf, e = stats.chi2_contingency(observed)
+
+    print('\n\n----------------------------------------------------------------------------------------------------\n')
+    
+    print(f'Chi2 Statistic: {chi2:.2f}\n')
+    formatted_p = "{:.{precision}e}".format(p, precision=decimal_places)
+    print(f'P-Value: {formatted_p}\n')
+    # print(f'Degrees of Freedom: {degf}\n')
+    # print(f'Expected: {e}\n')
+
+    # Plotting the countplot
+    title = f'{cat_var.capitalize()} vs. {target.capitalize()}'
+    sns.countplot(data=df, x=cat_var, hue=target)
+    plt.title(title)
+    plt.show()
+
+    eval_p(p)
+
+    print('\n\n\n')
+
+def analysis_1(df, cat_var, target, a=0.05):
+    """
+    Perform chi-squared test and visualize the results for quality vs. alcohol.
+
+    Args:
+        df (DataFrame): The DataFrame containing the data.
+        cat_var (str): The categorical variable (alcohol bins) to be tested.
+        target (str): The target variable (wine quality) for the chi-squared test.
+        a (float, optional): The significance level (default is 0.05).
+
+    Returns:
+        None
+    """
+    chi2_and_visualize(df, cat_var, target, a=0.05)
--- wrangle.py
+++ wrangle.py
@@ -27,16 +27,22 @@ def clean_wine():
     Cleans the wine dataset by converting column names to lowercase and replacing spaces with underscores.
 
     Returns:
-        pd.DataFrame: A cleaned DataFrame with standardized column names.
+        pd.DataFrame: A cleaned DataFrame with standardized column names and additional bins columns.
     """
+
     # Call acquire_wine to get the DataFrame
     df = acquire_wine()
 
     if df is not None:
         # Clean the column names
-        df.columns = [col.lower().replace(' ','_') for col in df.columns]
+        df.columns = [col.lower().replace(' ', '_') for col in df.columns]
+
+        # Create 'alcohol_bins' and 'quality_bins' columns
+        df['alcohol_bins'] = pd.cut(df['alcohol'], [0, 8, 10, 12, 15], labels=['no_alcohol', 'low_alcohol', 'medium_alcohol', 'high_alcohol'])
+        df['quality_bins'] = pd.cut(df['quality'], [0, 3, 7, 10], labels=['low', 'medium', 'high'])
 
     return df
+
 
 def train_val_test(df, target=None, seed = 42):
     """
@@ -54,49 +60,49 @@ def train_val_test(df, target=None, seed = 42):
     val, test = train_test_split(val_test, train_size=0.5, random_state=seed, stratify=target)
     return train, val, test
 
-def scale_data(train, val, test, scaler):
-    """
-    Scales the numerical columns of the data using the specified scaler.
+# def scale_data(train, val, test, scaler):
+#     """
+#     Scales the numerical columns of the data using the specified scaler.
 
-    Args:
-        train (pd.DataFrame): Training data.
-        val (pd.DataFrame): Validation data.
-        test (pd.DataFrame): Test data.
-        scaler (sklearn.preprocessing.Scaler): The scaler to use for data scaling.
+#     Args:
+#         train (pd.DataFrame): Training data.
+#         val (pd.DataFrame): Validation data.
+#         test (pd.DataFrame): Test data.
+#         scaler (sklearn.preprocessing.Scaler): The scaler to use for data scaling.
 
-    Returns:
-        tuple: A tuple containing scaled versions of train, validation, and test DataFrames.
-    """
-    # Make copies for scaling
-    train_scaled = train.copy()
-    val_scaled = val.copy()
-    test_scaled = test.copy()
+#     Returns:
+#         tuple: A tuple containing scaled versions of train, validation, and test DataFrames.
+#     """
+#     # Make copies for scaling
+#     train_scaled = train.copy()
+#     val_scaled = val.copy()
+#     test_scaled = test.copy()
 
-    columns_to_scale = ['fixed_acidity',
-                        'volatile_acidity',
-                        'citric_acid',
-                        'residual_sugar',
-                        'chlorides',
-                        'free_sulfur_dioxide',
-                        'total_sulfur_dioxide',
-                        'density',
-                        'ph',
-                        'sulphates',
-                        'alcohol',
-                        'quality',
-                       ]
+#     columns_to_scale = ['fixed_acidity',
+#                         'volatile_acidity',
+#                         'citric_acid',
+#                         'residual_sugar',
+#                         'chlorides',
+#                         'free_sulfur_dioxide',
+#                         'total_sulfur_dioxide',
+#                         'density',
+#                         'ph',
+#                         'sulphates',
+#                         'alcohol',
+#                         'quality',
+#                        ]
     
-    # Fit the scaler on the training data for all of the columns
-    scaler.fit(train[columns_to_scale])
+#     # Fit the scaler on the training data for all of the columns
+#     scaler.fit(train[columns_to_scale])
     
-    # Transform the data for each split
-    train_scaled[columns_to_scale] = scaler.transform(train[columns_to_scale])
-    val_scaled[columns_to_scale] = scaler.transform(val[columns_to_scale])
-    test_scaled[columns_to_scale] = scaler.transform(test[columns_to_scale])
+#     # Transform the data for each split
+#     train_scaled[columns_to_scale] = scaler.transform(train[columns_to_scale])
+#     val_scaled[columns_to_scale] = scaler.transform(val[columns_to_scale])
+#     test_scaled[columns_to_scale] = scaler.transform(test[columns_to_scale])
 
-    scaled_col = [train_scaled, val_scaled, test_scaled]
+#     scaled_col = [train_scaled, val_scaled, test_scaled]
     
-    return train_scaled, val_scaled, test_scaled
+#     return train_scaled, val_scaled, test_scaled
 
 def wrangle_wine():
     """
@@ -117,9 +123,9 @@ def wrangle_wine():
         train, val, test = train_val_test(df)
 
         # Scale the data
-        train_scaled, val_scaled, test_scaled = scale_data(train, val, test, mms)
+        # train_scaled, val_scaled, test_scaled = scale_data(train, val, test, mms)
 
-        return train_scaled, val_scaled, test_scaled
+        return df
     else:
         # Handle the case where data acquisition failed
         return None
