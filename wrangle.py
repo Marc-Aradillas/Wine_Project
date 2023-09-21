6c7abc0e9c148bd3c7a09266129d9fbc6f54b389
Marc Aradillas <aradillasm@gmail.com>
2023-09-21 14:38:45 -0500

    modified/created file

--- wrangle.py
+++ wrangle.py
@@ -142,49 +142,49 @@ def train_val_test(df, target=None, seed = 42):
     val, test = train_test_split(val_test, train_size=0.5, random_state=seed, stratify=target)
     return train, val, test
 
-# def scale_data(train, val, test, scaler):
-#     """
-#     Scales the numerical columns of the data using the specified scaler.
+def scale_data(train, val, test, scaler):
+    """
+    Scales the numerical columns of the data using the specified scaler.
 
-#     Args:
-#         train (pd.DataFrame): Training data.
-#         val (pd.DataFrame): Validation data.
-#         test (pd.DataFrame): Test data.
-#         scaler (sklearn.preprocessing.Scaler): The scaler to use for data scaling.
+    Args:
+        train (pd.DataFrame): Training data.
+        val (pd.DataFrame): Validation data.
+        test (pd.DataFrame): Test data.
+        scaler (sklearn.preprocessing.Scaler): The scaler to use for data scaling.
 
-#     Returns:
-#         tuple: A tuple containing scaled versions of train, validation, and test DataFrames.
-#     """
-#     # Make copies for scaling
-#     train_scaled = train.copy()
-#     val_scaled = val.copy()
-#     test_scaled = test.copy()
+    Returns:
+        tuple: A tuple containing scaled versions of train, validation, and test DataFrames.
+    """
+    # Make copies for scaling
+    train_scaled = train.copy()
+    val_scaled = val.copy()
+    test_scaled = test.copy()
 
-#     columns_to_scale = ['fixed_acidity',
-#                         'volatile_acidity',
-#                         'citric_acid',
-#                         'residual_sugar',
-#                         'chlorides',
-#                         'free_sulfur_dioxide',
-#                         'total_sulfur_dioxide',
-#                         'density',
-#                         'ph',
-#                         'sulphates',
-#                         'alcohol',
-#                         'quality',
-#                        ]
+    columns_to_scale = ['fixed_acidity',
+                        'volatile_acidity',
+                        'citric_acid',
+                        'residual_sugar',
+                        'chlorides',
+                        'free_sulfur_dioxide',
+                        'total_sulfur_dioxide',
+                        'density',
+                        'ph',
+                        'sulphates',
+                        'alcohol',
+                        'quality',
+                       ]
     
-#     # Fit the scaler on the training data for all of the columns
-#     scaler.fit(train[columns_to_scale])
+    # Fit the scaler on the training data for all of the columns
+    scaler.fit(train[columns_to_scale])
     
-#     # Transform the data for each split
-#     train_scaled[columns_to_scale] = scaler.transform(train[columns_to_scale])
-#     val_scaled[columns_to_scale] = scaler.transform(val[columns_to_scale])
-#     test_scaled[columns_to_scale] = scaler.transform(test[columns_to_scale])
+    # Transform the data for each split
+    train_scaled[columns_to_scale] = scaler.transform(train[columns_to_scale])
+    val_scaled[columns_to_scale] = scaler.transform(val[columns_to_scale])
+    test_scaled[columns_to_scale] = scaler.transform(test[columns_to_scale])
 
-#     scaled_col = [train_scaled, val_scaled, test_scaled]
+    scaled_col = [train_scaled, val_scaled, test_scaled]
     
-#     return train_scaled, val_scaled, test_scaled
+    return train_scaled, val_scaled, test_scaled
 
 def wrangle_wine():
     """
@@ -205,7 +205,7 @@ def wrangle_wine():
         train, val, test = train_val_test(df)
 
         # Scale the data
-        # train_scaled, val_scaled, test_scaled = scale_data(train, val, test, mms)
+        train_scaled, val_scaled, test_scaled = scale_data(train, val, test, mms)
 
         return train, val, test
     else:
